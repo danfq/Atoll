8402813df52a4e42560e653f979364389e6f62f7
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-11-01 10:32:51 +0530

    Initial Commit

---
+++ DynamicIsland/DynamicIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandApp.swift
+//  DynamicIsland
+//
+//  Created by admin63 on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
